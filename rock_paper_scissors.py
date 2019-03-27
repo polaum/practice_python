34af5f26415bcad4b4724fa1d6d9cc9230a271e0
Pola Umnitzin <polaumnitzin@MatanS-MBP.fritz.box>
2019-03-27 12:53:49 +0100

    completed step 1 of RPS

--- rock_paper_scissors.py
+++ rock_paper_scissors.py
@@ -1,1 +1,46 @@
-options = ["Rock", "Paper", "Scissors"]
+import random
+
+options = {"Rock": 1, "Paper": 2, "Scissors": 3}
+
+
+class SameChoiceException(Exception):
+    pass
+
+
+def choose_winner(my_choice):
+    winner = ''
+    cmp_choice = random.randint(1, len(options))
+    if my_choice == cmp_choice:
+        raise SameChoiceException("It's a Tie!")
+    elif my_choice == 1 and cmp_choice == 2:
+        winner = 'Computer'
+    elif my_choice == 1 and cmp_choice == 3:
+        winner = 'You'
+    elif my_choice == 2 and cmp_choice == 1:
+        winner = 'You'
+    elif my_choice == 2 and cmp_choice == 3:
+        winner = 'Computer'
+    elif my_choice == 3 and cmp_choice == 1:
+        winner = 'Computer'
+    elif my_choice == 3 and cmp_choice == 2:
+        winner = 'You'
+    else:
+        return winner
+
+
+def play():
+    while True:
+        choice = input("Welcome to Rock, Paper and Scissors game! \n"
+                       "Choose one of the options: \n"
+                       "1. Rock \n"
+                       "2. Paper \n"
+                       "3. Scissors \n"
+                        "To quit, type \"quit\" \n")
+        if choice == 'quit':
+            break
+        else:
+            winner = choose_winner(int(choice))
+            print(f"The winner is: {winner}")
+
+
+play()
